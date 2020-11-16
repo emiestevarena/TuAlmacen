17cc4b13fb4daac7113ef9b22e540b3dd90d6280
CristianRivera1 <72949520+CristianRivera1@users.noreply.github.com>
2020-11-16 18:41:13 -0300

    Add files via upload

---
+++ TuAlmacen/src/main/java/com/egg/TuAlmacen/TuAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.egg.TuAlmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuAlmacenApplication.class, args);
+	}
+
+}
