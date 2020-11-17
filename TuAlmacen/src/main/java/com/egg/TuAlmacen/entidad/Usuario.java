01f72ed3a72f78c3c585b2c21ec9402a676e21ac
Cristian Rivera <cristian@costaflores.com>
2020-11-16 21:29:15 -0300

    commit cristian

--- TuAlmacen/src/main/java/com/egg/TuAlmacen/entidad/Usuario.java
+++ TuAlmacen/src/main/java/com/egg/TuAlmacen/entidad/Usuario.java
@@ -1,5 +1,47 @@
 package com.egg.TuAlmacen.entidad;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
 public class Usuario {
-
+ @Id	
+ private String id;
+ private String usuario;
+ private String password;
+ private String email;
+ @OneToOne
+ private Rol rol;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getUsuario() {
+	return usuario;
+}
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public Roles getRoles() {
+	return roles;
+}
+public void setRoles(Roles roles) {
+	this.roles = roles;
+}
+ 
+ 
 }
