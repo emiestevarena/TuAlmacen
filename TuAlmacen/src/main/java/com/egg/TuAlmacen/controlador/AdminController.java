80fec7a9be316bd0b6ef0802b49468907309d0f3
emiestevarena <emiliano.estevarena@gmail.com>
2020-11-18 18:00:34 -0300

    admincontroller

---
+++ TuAlmacen/src/main/java/com/egg/TuAlmacen/controlador/AdminController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egg.TuAlmacen.controlador;
+
+import com.egg.TuAlmacen.entidad.Pedido;
+import com.egg.TuAlmacen.service.PedidoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author emiliano
+ */
+@Controller
+@RequestMapping("/")
+public class AdminController {
+    
+    @Autowired
+    private PedidoService pedidoService;
+    
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin")
+    public String admin(ModelMap modelo){
+        List<Pedido> pendientes = pedidoService.pendientes();
+        modelo.put("pendientes",pendientes);
+        return "admin.html";
+    }
+}
