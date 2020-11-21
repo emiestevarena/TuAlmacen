bea78d1a84ba409af6cfcf6a73167585c3b99e29
Josué Solís <josue1dario2solis3@gmail.com>
2020-11-20 21:29:15 -0300

    comentario controller

--- TuAlmacen/src/main/java/com/egg/TuAlmacen/repositorio/ComentarioRepositorio.java
+++ TuAlmacen/src/main/java/com/egg/TuAlmacen/repositorio/ComentarioRepositorio.java
@@ -1,13 +1,25 @@
 package com.egg.TuAlmacen.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.egg.TuAlmacen.entidad.Comentario;
+import com.egg.TuAlmacen.entidad.Producto;
+
 
 
 
 @Repository
 public interface ComentarioRepositorio extends JpaRepository<Comentario,String>{
 
+	
+	@Query("SELECT c FROM Comentario c, IN(c.producto) p WHERE p.id LIKE :id")
+    public List<Comentario>comentarioporproducto(@Param("id") String id);
+	
+	
 }
+
